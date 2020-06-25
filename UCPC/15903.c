d5165735a791948d31fc70ba514dd7ca34fa5daf
michaelsong93 <ss7277@seas.upenn.edu>
2020-06-25 23:18:39 +0900

    file modified

--- UCPC/15903.c
+++ UCPC/15903.c
@@ -2,20 +2,25 @@
 #include <stdlib.h>
 
 int cmpfunc (const void * a, const void * b) {
-    return ( *(int*)a - *(int*)b );
-    }
+    if( *(long long*)a == *(long long*)b )
+        return 0;
+    else if(*(long long*)a > *(long long*)b)
+        return 1;
+    else
+        return -1;
+}
 
 int main(){
     int N,M;
     scanf("%d%d", &N,&M);
 
-    int arr[1005];
+    long long arr[1005];
     for(int i = 0; i < N; i++){
         scanf("%lld", &arr[i]);
     }
 
     for(int i = 0; i < M; i++){
-        qsort(arr,N,sizeof(int), cmpfunc);
+        qsort(arr,N,sizeof(long long), cmpfunc);
         long long minimum1 = arr[0];
         long long minimum2 = arr[1];
         long long temp2 = minimum1 + minimum2;
