a6cf875326c8068aa590572915ced8e084b200b6
michaelsong93 <ss7277@seas.upenn.edu>
2020-07-04 22:53:35 +0900

    file update

--- Implementation-Algo/5533v1.c
+++ Implementation-Algo/5533v1.c
@@ -4,27 +4,42 @@ int main(){
     int N;
     scanf("%d", &N);
 
-    int arr[N][3];
+    int arr[3][N];
+    int score[3][N];
     for(int i = 0; i < N; i++){
         for(int j = 0; j < 3; j++){
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &arr[j][i]);
+            score[j][i] = arr[j][i];
         }
     }
 
     for(int i = 0; i < 3; i++){
+
         for(int j = 0; j < N; j++){
-            if(arr[i][j] == arr[i][j+1]){
-                arr[i][j] = 0;
+            for(int k = j+1; k < N; k++){
+                if(arr[i][j] == arr[i][k]){
+                    score[i][j] = 0;
+                    score[i][k] = 0;
+                }
             }
         }
-    }
 
+    }  
+    
+    int sum = 0;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < 3; j++){
-            printf("%d ", arr[i][j]);
+            sum += score[j][i];
         }
-        printf("\n");
+        printf("%d\n", sum);
+        sum = 0;
     }
+    // for(int i = 0; i < N; i++){
+    //     for(int j = 0; j < 3; j++){
+    //         printf("%d ", arr[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
 
 
