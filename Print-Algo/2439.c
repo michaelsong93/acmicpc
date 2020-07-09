9ff17a31c277c07a2393c21e5a8e8bc3c844b707
michaelsong93 <ss7277@seas.upenn.edu>
2020-07-09 20:29:16 +0900

    file upload

---
+++ Print-Algo/2439.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            printf(" ");
+        }
+
+        for(int k = 0; k <= i; k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
