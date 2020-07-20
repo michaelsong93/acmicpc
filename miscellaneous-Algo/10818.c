4ca920e185120e78eb129502519355a36db36560
michaelsong93 <ss7277@seas.upenn.edu>
2020-07-20 16:25:22 +0900

    file upload

---
+++ miscellaneous-Algo/10818.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int N,min = 1000000,max = -1000000;
+    scanf("%d", &N);
+    
+    int arr[N];
+    for(int i = 0; i < N; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < N; i++){
+        if(arr[i] <= min){
+            min = arr[i];
+        }
+
+        if(arr[i] >= max){
+            max = arr[i];
+        }
+    }
+
+    printf("%d %d", min, max);
+}
