337582cb7f4452fa80c27584299633cc4e1235a7
michaelsong93 <ss7277@seas.upenn.edu>
2020-08-03 20:53:45 +0900

    file upload

--- Math-Algo/1978v1.c
+++ Math-Algo/1978v1.c
@@ -7,14 +7,22 @@ int main(){
 
     for(int i = 0; i < N; i++){
         scanf("%d", &num);
+        if(num == 1){
+            continue;
+        }
+
+        int flag = 1;
         for(int j = 2; j <= sqrt(num); j++){
+            //j*j <= num
             if(num % j == 0){
+                flag = 0;
                 break;
             }
-            else{
-                count++;
-            }
         }
+        if(flag == 1){
+            count++;
+        }
+        //flag = 1;
     }
 
     printf("%d", count);
