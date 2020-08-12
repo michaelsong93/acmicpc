5bd6f2e28828cab1fc9bb6bd4cc6e4fb6b987875
michaelsong93 <ss7277@seas.upenn.edu>
2020-08-12 19:27:09 +0900

    file upload

--- miscellaneous-Algo/2941v1.c
+++ miscellaneous-Algo/2941v1.c
@@ -5,46 +5,43 @@ int main(){
     char character [105];
     scanf("%[^\n]s", character);
     int count = 0;
-    for(int i = 0; i < strlen(character); i++){
-        for(int j = i+1; j < i+1; j++){
-            if(character[i] == 'c' && character[j] == '='){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 'c' && character[j] == '-'){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 'd' && character[j] == '-'){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 'l' && character[j] == 'j'){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 'n' && character[j] == 'j'){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 's' && character[j] == '='){
-                count++;
-                i = j + 1;
-            }
-            else if(character[i] == 'z' && character[j] == '='){
-                count++;
-                i = j + 1;
-            }
-            // for(int k = j + 1; k < strlen(character); k++){
-            //     if(character[i] == 'd' && character[j] == 'z' && character[k] == '='){
-            //         count++;
-            //         i = k + 1;
-            //         continue;
-            //     }
-            // }
-
+    for(int i = 0; character[i] != '\0';){
+        if(character[i] == 'c' && character[i+1] == '='){
+            count++;
+            i = i + 2;
         }
-        count++;
+        else if(character[i] == 'c' && character[i+1] == '-'){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 'd' && character[i+1] == '-'){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 'l' && character[i+1] == 'j'){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 'n' && character[i+1] == 'j'){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 's' && character[i+1] == '='){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 'z' && character[i+1] == '='){
+            count++;
+            i = i + 2;
+        }
+        else if(character[i] == 'd' && character[i+1] == 'z' && character[i+2] == '='){
+            count++;
+            i = i + 3;
+        }
+        else{
+            i++;
+            count++;
+        }
     }
-    printf("%d", count);
+     printf("%d", count);
 }
