a0b6b0a2b06c4fbd1523b9c02e22d03ba55505b7
michaelsong93 <ss7277@seas.upenn.edu>
2020-08-12 19:26:13 +0900

    file upload

---
+++ Simulation-Algo/2164v1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int Card;
+    scanf("%d", &Card);
+    int arr[Card];
+    int ans[Card];
+    
+    for(int i = 0; i < Card; i++){
+        arr[i] = i + 1;
+    }
+
+    for(int i = 0; i < Card; i++){
+        ans[i] = arr[i];
+        int temp = arr[i+1];
+    
+        for(int j = i+1; j < Card; j++){
+            arr[j] = arr[j+1];
+        }
+        arr[Card-1] = temp;
+    }
+
+    printf("%d", ans[Card-1]);
+}
